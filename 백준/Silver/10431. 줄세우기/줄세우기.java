68c4fe445841601eb3dcb186c53eeba7c4cfc68b
zoomin99 <113499373+zoomin99@users.noreply.github.com>
2023-09-18 22:47:21 +0900

    [Silver V] Title: 줄세우기, Time: 556 ms, Memory: 38760 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,31 +1,30 @@
 import java.util.Scanner;
 
-class Main
-{
-    public static void main (String[] args) {
+class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int P = sc.nextInt();
-        while (P-- > 0) {
-            int T = sc.nextInt();
-            int[] h = new int[20];
-            for (int i = 0; i < 20; i++)
-                h[i] = sc.nextInt();
 
-            int cnt = 0;
+        int totalTest = sc.nextInt();
+
+        for (int test = 1; test <= totalTest; test++) {
+            int testNum = sc.nextInt();
+            int studentArr[] = new int[20];
+            int count = 0;
             for (int i = 0; i < 20; i++) {
+                studentArr[i] = sc.nextInt();
                 for (int j = 0; j < i; j++) {
-                    if (h[j] > h[i]) {
-                        int myH = h[i];
-                        for (int k = i; k > j; k--) {
-                            h[k] = h[k - 1];
-                            cnt++;
+                    if (studentArr[j] > studentArr[i]) {
+                        int currentStudent = studentArr[i];
+                        for (int k = i - 1; k >= j; k--) {
+                            studentArr[k + 1] = studentArr[k];
+                            count++;
                         }
-                        h[j] = myH;
+                        studentArr[j] = currentStudent;
                         break;
                     }
                 }
             }
-            System.out.println(T + " " + cnt);
+            System.out.println(test + " " + count);
         }
     }
 }
